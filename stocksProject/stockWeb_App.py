55cb34a25abfc28424eabfa8a1236c28037d746e
itzVishal21 <74182029+itzVishal21@users.noreply.github.com>
2021-04-13 12:59:13 +0530

    Update stockWeb_App.py

--- stocksProject/stockWeb_App.py
+++ stocksProject/stockWeb_App.py
@@ -1,12 +1,13 @@
+#This is a Stocks Web Appliction
+#Importing Libraries
 from datetime import datetime
 from typing import Union
-
 import streamlit as st
 import pandas as pd
 from PIL import Image
 from pandas import DatetimeIndex, Series
 from pandas._libs.tslibs.nattype import NaTType
-
+#Add Title And Image
 st.write("""
 # Stock Market Web Application
 **Visually** show data on a stock! 
@@ -14,15 +15,15 @@ st.write("""
 
 image = Image.open("C:/Users/Vishal/Desktop/stocksProject/pro/icon.jpg")
 st.image(image, use_column_width= True)
-
+#create sidebar header
 st.sidebar.header('User Input')
-
+#creating function to get user input
 def get_input():
     start_date= st.sidebar.text_input("Start Date", "01-01-2018")
     end_date= st.sidebar.text_input("End Date", "01-01-2021")
     stock_symbol= st.sidebar.text_input("Stock Symbol", "AMZN")
     return start_date, end_date, stock_symbol
-
+#creating function to get company name
 def get_comp_name(symbol):
     if symbol == "AMZN":
         return 'Amazon'
@@ -32,9 +33,9 @@ def get_comp_name(symbol):
         return 'Google'
     else:
         'None'
-
+#creating fuction to get proper company data and proper time frame from the user start date to the users end date
 def get_data(symbol, start, end):
-
+#Load Data
     if symbol.upper() == 'AMZN':
         df = pd.read_csv("C:/Users/Vishal/Desktop/stocksProject/csv/AMZN.csv")
     elif symbol.upper() == 'TSLA':
@@ -43,10 +44,10 @@ def get_data(symbol, start, end):
         df = pd.read_csv("C:/Users/Vishal/Desktop/stocksProject/csv/GOOG.csv")
     else:
         df = pd.DataFrame(columns= ['Date', 'Close', 'Open', 'Volume', 'Adj close', 'High', 'Low'])
-
+#get the date range
     start = pd.to_datetime(start)
     end = pd.to_datetime(end)
-
+#Sent the start and end index row both to 0
     start_row = 0
     end_row = 0
 
